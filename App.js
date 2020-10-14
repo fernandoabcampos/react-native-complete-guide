1aa1877261e8c922ed2ca09322a611c7a6bc4ca0
Fernando Barbeiro <fernandoabcampos@gmail.com>
2020-10-14 22:29:37 +0200

    Creating a simple "Goal List" with basic react native components

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Text,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      {
+        uid: Math.random().toString(),
+        value: enteredGoal,
+      },
+    ]);
+  };
+
+  const clearGoalHandler = () => {
+    setCourseGoals([]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="ADD" onPress={addGoalHandler} />
+        {
+          // <Button title="CLEAR" onPress={clearGoalHandler} />
+        }
+      </View>
+      <FlatList
+        data={courseGoals}
+        keyExtractor={(item, index) => item.uid}
+        renderItem={(itemData) => (
+          <View style={styles.listItems}>
+            <Text>{itemData.item.value}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
+    padding: 50,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderBottomColor: "black",
+    borderBottomWidth: 0.8,
+    borderWidth: 0.3,
+    padding: 10,
+  },
+  listItems: {
+    padding: 10,
+    backgroundColor: "#ccc",
+    borderColor: "grey",
+    borderWidth: 0.5,
+    marginVertical: 10,
   },
 });
---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-complete-guide
+Learning some react native
